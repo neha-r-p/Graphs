372f98e1b54921749057b64f692d080c8cb0d525
Neha Patel <neha.r.p@gmail.com>
2020-01-21 21:56:36 -0800

    ancestor.py passing tests, woohoo MVP

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -12,6 +12,28 @@ def earliest_ancestor(ancestors, starting_node):
             graph.add_vertex(ancestor[1])
         # print("parent", ancestor[0], "child", ancestor[1])
         graph.add_edge(ancestor[1], ancestor[0])
-    
-    print("graph", graph.vertices)
+
+    #if no "parent" then return -1
+    if graph.vertices[starting_node] == set():
+        return -1
+    else:
+        queue = Queue()
+
+        queue.enqueue([starting_node])
+        
+        visited = set()
+
+        while queue.size() > 0:
+            path = queue.dequeue()
+            vertex = path[-1]
+            if vertex not in visited:
+                if graph.vertices[vertex] == set():
+                    oldest_ancestor = path[-1]
+            visited.add(vertex)
+
+            for next_vert in graph.get_neighbors(vertex):
+                new_path = list(path)
+                new_path.append(next_vert)
+                queue.enqueue(new_path)
+        return oldest_ancestor
 
