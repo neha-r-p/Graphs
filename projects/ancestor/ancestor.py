d1e1f6e35cd8e5e25702b874a310fb00c607c45a
Neha Patel <neha.r.p@gmail.com>
2020-01-21 16:09:58 -0800

    adding vertices to graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,5 +1,15 @@
 from util import Stack, Queue
-from graph inport Graph
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    #ancestors will be a list of tuples?
+    # print("ancestor print", ancestors)
+    graph = Graph()
+    for ancestor in ancestors:
+        if ancestor[0] not in graph.vertices:
+            graph.add_vertex(ancestor[0])
+        if ancestor[1] not in graph.vertices:
+            graph.add_vertex(ancestor[1])
+        # print("parent", ancestor[0], "child", ancestor[1])
+    print("graph vertices", graph.vertices)
+
